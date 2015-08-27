6433308e321f6d9bfa8f8f17c598258957dda4c6
unknown <GoncharovDmV@vdg.corp.lukoil.com>
2015-08-27 11:46:43 +0300

    Create table Posts

--- app.rb
+++ app.rb
@@ -3,16 +3,13 @@ require 'sinatra'
 require 'sqlite3'
 
 
-def init_db 
-
+def init_db
 @db = SQLite3::Database.new 'leprosorium.db'
 @db.results_as_hash = true
-
 end
 
 before do
-
-
+init_db
 end
 
 get '/' do
